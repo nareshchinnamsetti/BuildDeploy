27d426fb49d91bea1d3389f5e8cc8b2ff42cab21
nareshchinnamsetti <33509518+nareshchinnamsetti@users.noreply.github.com>
2019-03-17 21:55:19 +0530

    Update vars.tf

--- vars.tf
+++ vars.tf
@@ -1,14 +1,14 @@
 variable "access_key" {
   description = "The AWS access key."
-  default = "AKIAJNJJWYQ2ABALEIOQ"
+  default = ""
 }
 
 variable "secret_key" {
   description = "The AWS secret key."
-  default = "5950D+1Jp5yFhGN0Nyq1V+3CfECMcW8Z+sz2NpHI"
+  default = ""
 }
 
 variable "region" {
   description = "The AWS region to create resources in."
-  default = "us-east-1"
+  default = ""
 }
